1373d62686082bebf87d92fa806ce53fa3db88cf
taif01 <taifahmedshemul@gmail.com>
2024-09-21 04:53:04 +0600

    add and set value to UI/DOM

--- js/home.js
+++ js/home.js
@@ -13,4 +13,25 @@ document.getElementById('btn-add-money')
         // get the pin number provided
         const pinNumberInput = document.getElementById('input-pin-number').value;
         console.log(pinNumberInput);
+
+        // verify pin number(Wrong way to verify pin number)
+        if(pinNumberInput === '1234'){
+            console.log('Adding money to your account');
+
+            // get the current balance
+            const balance = document.getElementById('account-balance').innerText;
+            console.log(balance);
+
+            // step 5: add money with balance
+            const addMoneyNumber = parseFloat(addMoneyInput);
+            const balanceNumber = parseFloat(balance);
+            const newBalance = addMoneyNumber + balanceNumber;
+            console.log(newBalance);
+
+            // step-6: update the balance in UI/DOM
+            document.getElementById('account-balance').innerText = newBalance;
+        }
+        else{
+            alert('Failed to add money! Please try again.')
+        }
 })
